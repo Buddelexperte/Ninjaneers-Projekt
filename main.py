47fc7b28d0392e19e06250b66b821a5160948999
Carl <clawwshadoww@gmail.com>
2025-06-23 13:32:56 +0200

    ...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-url = "mysql+pymysql://root@localhost:3306/weather_data"
+url = "mysql+pymysql://root:@localhost:3306/weather_data"
 
 engine = create_engine(url, echo=True)
 
