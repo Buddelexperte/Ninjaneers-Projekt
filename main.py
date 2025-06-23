258e5ab6f0bdcbb57c4b01a79177c9f4eea47333
danylo-ninjaneers <danylo.kushniruk@ninjaneers.de>
2025-06-23 15:08:22 +0200

    Implemented databse fixes

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
+# Session creation
+url = "mysql+pymysql://root:password@localhost:3306/weather_data"
+engine = create_engine(url, echo=True)
+session = Session(engine)
+
 # Importing types
 from sqlalchemy import Table, Column, Integer, String, Date, Float
 from datetime import date
@@ -20,10 +25,7 @@ class WeatherInfo (Base) :
     wind = Column(Float)
     weather= Column(String(10))
 
-# Session creation
-url = "mysql+pymysql://root:@localhost:3306/weather_data"
-engine = create_engine(url, echo=True)
-session = Session(engine)
+Base.metadata.create_all(engine)
 
 # New Sets
 new_set = WeatherInfo(date = date(2025, 7, 26), precipitation= 2.2, temp_max = 24.3, temp_min= 0.2, wind= 0.1, weather= "sun" )
