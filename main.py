6a4115be550444eddd38f058490aa32dbb71e79e
danylo-ninjaneers <danylo.kushniruk@ninjaneers.de>
2025-06-23 14:48:24 +0200

    comments removed and added for readability

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
+# Importing Engine and Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import MetaData
-metadata_obj = MetaData()
+
+# Importing types
+from sqlalchemy import Table, Column, Integer, String, Date, Float
 from datetime import date
 
-from sqlalchemy import Table, Column, Integer, String, Date, Float
-
-# weather_table = Table(
-  #  "weather_info",
-   # metadata_obj,
-    #Column("id", Integer, primary_key=True),
-    #Column("date", Date),
-    #Column("precipitation", Float),
-    #Column("temp_max", Float),
-    #Column("temp_min", Float),
-    #Column("wind", Float),
-    #Column("weather", String(10)),
-#)
-
+from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 class WeatherInfo (Base) :
@@ -32,18 +20,16 @@ class WeatherInfo (Base) :
     wind = Column(Float)
     weather= Column(String(10))
 
+# Session creation
 url = "mysql+pymysql://user:passwort@localhost:3306/weather_data"
-
 engine = create_engine(url, echo=True)
-
-
 session = Session(engine)
 
-from sqlalchemy import insert
-#stmt = insert(weather_table).values(date="2015-01-01", precipitation=3.0, temp_max=1.0, temp_min=0.0, wind=0.0, weather="sun")
-
+# New Sets
 new_set = WeatherInfo(date = date(2025, 7, 26), precipitation= 2.2, temp_max = 24.3, temp_min= 0.2, wind= 0.1, weather= "sun" )
 
+# Adding sets as changes to session
 session.add(new_set)
 
+# Commiting changes
 session.commit()
