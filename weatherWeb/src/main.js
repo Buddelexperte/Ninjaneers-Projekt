171730cc89359021a56fa62907cfba035573629d
Buddelexperte <74837601+Buddelexperte@users.noreply.github.com>
2025-06-24 10:20:27 +0200

    minor changes in index.html

---
+++ weatherWeb/src/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const message = document.getElementById("message");
+message.textContent = "You just triggered HMR!";
+
+// Example: log a message to the console
+console.log("Vite HMR is active");
