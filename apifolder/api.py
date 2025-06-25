70b61419513ae6f48dc1a5b8b6d05e6903bf7f34
Carl <clawwshadoww@gmail.com>
2025-06-25 11:35:36 +0200

    Diagrams are saved with automatic name, pictures are returned

--- apifolder/api.py
+++ apifolder/api.py
@@ -1,9 +1,10 @@
-from datetime import date
-
+from datetime import date, timedelta, datetime
+import matplotlib.pyplot as max_temp_diagram
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from src.settings import Engine, WeatherInfo, Session
 from sqlalchemy import select, extract
+import base64
 
 
 router = APIRouter()
@@ -19,6 +20,7 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/weather/all")
 async def get_info():
@@ -36,10 +38,55 @@ async def get_info(year: int):
         res = session.execute(statement).mappings().all()
     return res
 
-@app.get("/weather/{date1}-{date2}")
-async def get_info(date1 : date, date2 : date):
-    text :str
-    with Session(Engine) as session:
-        #statement = select(WeatherInfo).where(extract('year', WeatherInfo.date) == year)
-        res = session.execute(statement).mappings().all()
-    return res
+
+@app.get("/weather/timeframe/{i_startDate}/{i_endDate}")
+async def get_info(i_startDate : str, i_endDate : str):
+    startYear = i_startDate[0:4]
+    startMonth = i_startDate[5:7]
+    startDay = i_startDate[8:10]
+    startDate = date(int(startYear), int(startMonth), int(startDay))
+
+    endYear = i_endDate[0:4]
+    endMonth = i_endDate[5:7]
+    endDay = i_endDate[8:10]
+    endDate = date(int(endYear), int(endMonth), int(endDay))
+
+    days = []
+    temps = []
+
+    current_date = startDate
+    index = 0
+
+
+    while current_date <= endDate:
+        days.append(current_date.strftime("%Y-%m-%d"))
+        #print("Test1")
+
+        with Session(Engine) as session:
+            stmt = select(WeatherInfo.temp_max).where(WeatherInfo.date == current_date)
+            maximum_temp = session.execute(stmt).mappings().first()
+            temps.append(maximum_temp['temp_max'])
+
+            index += 1
+            #print(maximum_temp)
+            current_date += timedelta(days=1)
+
+    max_temp_diagram.bar(days, temps)
+    max_temp_diagram.title("Maximale Temperatur vom " + str(startDate) + " bis zum " + str(endDate))
+
+    max_temp_diagram.xlabel('Tage')
+    max_temp_diagram.ylabel('Temperatur in Grad')
+
+    max_temp_diagram.xticks(rotation=90)
+
+    max_temp_diagram.tight_layout()
+
+
+    now = datetime.now()
+    max_temp_diagram.savefig('images/' + str(now) + '.png')
+
+
+    with open("images/" + str(now) + ".png", "rb") as img_file:
+        encoded_string = base64.b64encode(img_file.read()).decode('utf-8')
+    return {"filename": str(now) + ".png", "image_base64": f"data:image/png;base64,{encoded_string}"}
+
